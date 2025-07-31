53a3cd3861138c9a96715d63e76333795a4c847a
nanahaya770 <asyaiadam@mail.ru>
2025-07-31 20:47:40 +0300

    функция проверяет поиск

---
+++ test/test_ui.py
@@ -0,0 +1,43 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pytest
+
+UNKNOWN_BOOK = "рмюлмгюша"
+
+
+@pytest.fixture
+def driver():
+    service = ChromeService(ChromeDriverManager().install())
+    driver: WebDriver = webdriver.Chrome(service=service)
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+
+def test_search_box(driver):
+    """
+    Функция проверят поис книги
+    """
+    driver.get("https://www.chitai-gorod.ru")
+
+    search_box = driver.find_element(By.NAME, "search")
+    search_box.clear()
+    search_box.send_keys(UNKNOWN_BOOK)
+    driver.find_element(
+        By.XPATH,
+        '//*[@id="__nuxt"]/div/header/div/div[1]/div/div/div[1]/form/button'
+    ).click()
+
+    element = WebDriverWait(driver, 3).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'search-title'))
+    )
+    assert element is not None, "Элемент не найден на странице"
+    assert element.text == (
+        f'Поиск по запросу «{UNKNOWN_BOOK}» не принёс результатов'
+    )
