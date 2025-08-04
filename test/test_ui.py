449c17adb5332eb87fd12356427f66679fb70ece
nanahaya770 <asyaiadam@mail.ru>
2025-08-04 19:14:52 +0300

    5 тестов

--- test/test_ui.py
+++ test/test_ui.py
@@ -13,6 +13,9 @@ UNKNOWN_BOOK = "рмюлмгюша"
 
 @pytest.fixture(scope='module')
 def driver():
+    """
+    Фикстура открывает браузер
+    """
     service = ChromeService(ChromeDriverManager().install())
     driver: WebDriver = webdriver.Chrome(service=service)
     driver.maximize_window()
@@ -44,7 +47,13 @@ def test_search_box(driver):
 
 
 def test_add_book_to_cart(driver):
-    driver.get("https://www.chitai-gorod.ru/product/bukvar-uchebnoe-posobie-94503")
+    """
+    Функция проверяет появление цыфры 1 у значка пустой корзины,
+    после нажатия на кнопку 'Купить'
+    """
+    driver.get(
+        "https://www.chitai-gorod.ru/product/bukvar-uchebnoe-posobie-94503"
+        )
     driver.find_element(
         By.XPATH, '//*[@id="__nuxt"]/div/div[3]/div[1]/div/main/aside/div[2]'
         '/header/div/div[2]/button[1]').click()
@@ -59,10 +68,69 @@ def test_add_book_to_cart(driver):
 
 
 def test_book_in_cart(driver):
+    """
+    Функция проверяет правильность подчета
+    итогового количества товара в корзине
+    """
     driver.get("https://www.chitai-gorod.ru/cart")
     count = WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR,
-        '#__nuxt > div > div.app-wrapper__content > div:nth-child(1) > div > div > div.cart-page__cart-content > div.cart-page__cart-content--right > div > div:nth-child(2) > section.cart-sidebar__info > div:nth-child(1) > div.info-item__title'))
+        EC.visibility_of_element_located((
+            By.CSS_SELECTOR,
+            '#__nuxt > div > div.app-wrapper__content > div:nth-child(1) > '
+            'div > div > div.cart-page__cart-content > '
+            'div.cart-page__cart-content--right > div > div:nth-child(2) > '
+            'section.cart-sidebar__info > '
+            'div:nth-child(1) > div.info-item__title'
+        ))
+    )
+
+    assert count.text == "1 товар"
+
+
+def test_empty_cart(driver):
+    """
+    Функция проверяет кнопку очистки корзины
+    """
+
+    driver.find_element(By.CLASS_NAME, 'cart-page__delete-many').click()
+
+    time.sleep(10)
+
+    empty_cart = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((
+            By.CLASS_NAME, 'cart-multiple-delete__title'
+        ))
+    )
+
+    assert empty_cart.text == "Корзина очищена"
+
+
+def test_recover_cart(driver):
+    """
+    Функция проверяет кнопку востановления корзины
+    """
+
+    button_recover = (
+        '#__nuxt > div > div.app-wrapper__content > div > '
+        'div > div > section > div > button.chg-app-button'
+        '.chg-app-button--primary.chg-app-button--s.'
+        'chg-app-button--brand-blue.cart-multiple-delete__button.'
+        'cart-multiple-delete__button'
+        )
+
+    driver.find_element(By.CSS_SELECTOR, button_recover).click()
+
+    time.sleep(2)
+
+    count = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((
+            By.CSS_SELECTOR,
+            '#__nuxt > div > div.app-wrapper__content > div:nth-child(1) > '
+            'div > div > div.cart-page__cart-content > '
+            'div.cart-page__cart-content--right > div > div:nth-child(2) > '
+            'section.cart-sidebar__info > '
+            'div:nth-child(1) > div.info-item__title'
+        ))
     )
 
     assert count.text == "1 товар"
